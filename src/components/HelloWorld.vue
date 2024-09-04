a6dc0787f76f3c8591b11d39ba941f7735ca2aeb
Nguyen9c1999 <110385707+Nguyen9c1999@users.noreply.github.com>
2024-09-04 14:06:20 +0700

    <update>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,44 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="p-2">
+    <h1 style="text-align: center" >TO DO LIST</h1>
+    <input type="text" class="mb-2" v-model="content">
+    <br>
+    <button type="button" class="btn btn-primary" @click="add()">Thêm</button>
+    <table class="table mt-2">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">content</th>
+        <th>Trạng thái</th>
+        <th scope="col">Handle</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(work,index) in lists" :key="index">
+        <th scope="row">{{index+1}}</th>
+        <td :class="{'strikethrough':work.status}">{{work.content}}</td>
+        <td>
+          <input type="checkbox" v-model="work.status">
+        </td>
+        <td>
+          <button type="button" class="btn btn-success" :disabled="work.status" @click="openModal(work,index)">Edit</button>
+          <button type="button" class="btn btn-danger ml-2" @click="deleteWork()">Xóa</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
   </div>
+  <el-dialog
+     v-model="isOpen"
+      title="Tips"
+      width="500"
+
+  >
+    <input type="text" class="mb-2" v-model="contentEdit">
+    <br>
+    <button type="button" class="btn btn-primary" @click="edit()">EDIT</button>
+  </el-dialog>
+
 </template>
 
 <script>
@@ -35,24 +46,49 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      lists:[],
+      isOpen:false,
+      content:'',
+      contentEdit:'',
+      indexEdit: null
+    }
+  },
+  methods:{
+    add(){
+      if(!this.content){
+       alert('vui lòng nhập to do list')
+        return
+      }
+      this.lists = [{content: this.content , status: false},...this.lists]
+      this.content = ''
+    },
+    edit(){
+      if(!this.contentEdit){
+        alert('vui lòng nhập to do list')
+        return
+      }
+      this.lists[this.indexEdit].content = this.contentEdit
+      this.isOpen = false;
+    },
+    deleteWork(index){
+      this.lists.splice(index,1)
+    },
+    openModal(work, index){
+      this.isOpen = true;
+      this.contentEdit = work.content;
+      this.indexEdit = index
+    }
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.strikethrough {
+  text-decoration: line-through;
 }
 </style>
